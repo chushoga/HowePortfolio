751b086b89e3ac011d0e75a785cd8e4c43dd22aa
chushoga <chushoga@gmail.com>
2017-04-21 08:40:21 +0900

    First Commit
    Setup some simple ai to test animations

---
+++ Assets/Scripts/AI/AICritter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICritter : MonoBehaviour {
+
+	[SerializeField] float wanderRange = 10f;
+	[SerializeField] float wanderSpeed = 2.5f;
+
+	public GameObject spawnPoint;
+
+	private Vector3 currentPos;
+	public Vector3 targetPos;
+
+	GameObject gm;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float step = wanderSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
+	}
+}
