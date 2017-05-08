87e4ec851cc6c156c835a3841668e0cf0d0ed85c
chushoga <chushoga@gmail.com>
2017-05-08 09:07:20 +0900

    Added to Simple AI code
    Added freeze rotation and require componet.

--- Assets/Scripts/AI/AICritter.cs
+++ Assets/Scripts/AI/AICritter.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 // requires a sphere collider for the collision detection.
 [RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(Rigidbody))]
 public class AICritter : MonoBehaviour {
 
 	Vector3 startPos; // starting position
@@ -14,10 +15,17 @@ public class AICritter : MonoBehaviour {
 	[SerializeField] float wanderRange = 10f;
 	[SerializeField] float wanderSpeed = 2.5f;
 
-
+	Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
+		// get Rigidbody
+		rb = GetComponent<Rigidbody>();
+
+		//lock rotaiton
+		rb.constraints = RigidbodyConstraints.FreezeRotation;
+
+
 		collisionBarrier = GetComponent<SphereCollider>(); // grab the sphere collider
 		startPos = this.transform.position; 
 		Debug.Log(startPos);
@@ -26,6 +34,6 @@ public class AICritter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float step = wanderSpeed * Time.deltaTime;
-		transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
+		//transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
 	}
 }
