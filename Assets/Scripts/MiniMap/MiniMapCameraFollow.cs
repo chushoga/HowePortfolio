2ce39d247b092bebb78300b6107fc14c3692d04f
chushoga <chushoga@gmail.com>
2017-09-21 09:01:29 +0900

    Minimap Camera Follow
    Added one script and unparented the minimap from the character02

---
+++ Assets/Scripts/MiniMap/MiniMapCameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCameraFollow : MonoBehaviour {
+
+	public GameObject camTarget;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = camTarget.transform.position;
+		pos.y += 50.0f;
+		transform.position = pos;
+	}
+}
