089973d7495bb45d98155b74deaf32682f1b67ab
chushoga <chushoga@gmail.com>
2017-09-24 16:08:53 +0900

    camera update

---
+++ Assets/Scripts/Movement/MMOCharacterController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MMOCharacterController : MonoBehaviour {
+
+	public Transform playerCam, character, centerPoint;
+
+	private float mouseX, mouseY;
+	public float mouseSensativity = 10.0f;
+
+	private float moveFB, moveLR;
+	public float moveSpeed = 2f;
+
+	private float zoom;
+	public float zoomSpeed = 2f;
+
+	public float zoomMin = -2f;
+	public float zoomMax = -10f;
+
+	// Use this for initialization
+	
+	void Start () {
+		
+		zoom = -3f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		zoom += Input.GetAxis("Mouse ScrollWheel") * 50f;
+
+
+		if(zoom > zoomMin) {
+			zoom = zoomMin;
+		}
+
+		if(zoom < zoomMin) {
+			zoom = zoomMax;
+		}
+
+		playerCam.transform.position = new Vector3(0, 0, zoom);
+
+	}
+}
