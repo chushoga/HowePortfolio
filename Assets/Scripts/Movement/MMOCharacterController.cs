6a7e4d95f1c5d69e253369606ed537d00e7e0fb4
chushoga <chushoga@gmail.com>
2017-09-26 09:54:46 +0900

    Work on Orthographic camera

--- Assets/Scripts/Movement/MMOCharacterController.cs
+++ Assets/Scripts/Movement/MMOCharacterController.cs
@@ -10,17 +10,23 @@ public class MMOCharacterController : MonoBehaviour {
 	float zoomMax = 5.0f;
 	float zoomOffset = 4.5f;
 
+	Vector3 cameraRotation;
+
 	void Start () {
+
+		// Get camera rotation
+		cameraRotation = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+		Debug.Log (cameraRotation);
 		
 		// start zoom at half of the max
 		Camera.main.orthographicSize = zoomMax / 2.0f;
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		transform.position = new Vector3(target.position.x, transform.position.y, target.position.z - zoomOffset);
+		transform.position = new Vector3(transform.position.x, transform.position.y, target.position.z - zoomOffset);
+		transform.eulerAngles = cameraRotation;
 		
 		if(Input.GetAxis("Mouse ScrollWheel") < 0) {
 			Camera.main.orthographicSize += 0.5f;
