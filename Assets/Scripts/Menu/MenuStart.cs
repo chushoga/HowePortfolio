b458dcc606bd658172d9abefab759a491dead32b
chushoga <chushoga@gmail.com>
2017-10-03 11:17:55 +0900

    Kinda Big update

---
+++ Assets/Scripts/Menu/MenuStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStart : MonoBehaviour {
+
+	public void changeMenuScene(string sceneName) {
+		SceneManager.LoadScene (sceneName);
+
+	}
+
+}
