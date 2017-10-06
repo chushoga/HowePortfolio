55f81a12b08d3d15373f7f474ec01959f383b575
chushoga <chushoga@gmail.com>
2017-10-06 09:19:55 +0900

    Model Preview Update UI

---
+++ Assets/Scripts/ModelPreview/ModelController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelController : MonoBehaviour
+{
+
+	public float rotSpeed = 2.0f; // rotation speed for the character
+
+	// ANIMATIONS
+	public RuntimeAnimatorController aniCont; // animation controller
+
+	// UI
+	public RectTransform parentPanel;
+	public GameObject actionButtonPrefab;
+
+	void Start()
+	{
+		
+		Debug.Log(parentPanel.childCount);
+		for(int i = 0; i < aniCont.animationClips.Length; i++) {
+			//Debug.Log(aniCont.animationClips[i].name);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed);
+
+	}
+}
